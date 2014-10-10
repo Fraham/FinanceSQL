1fffcd0a31be53e39c026c76fabbee5ca10ad801
Fraham <frahamp@gmail.com>
2014-10-10 01:51:42 +0100

    First Commit

---
+++ FinanceSQL/Bank.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceSQL
+{
+    class Bank
+    {
+        private int bankID;
+        private string name;
+
+        public Bank(int newBankID, string newName)
+        {
+            BankID = newBankID;
+            Name = newName;
+        }
+
+        public int BankID
+        {
+            get
+            {
+                return this.bankID;
+            }
+            set
+            {
+                this.bankID = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+    }
+}
